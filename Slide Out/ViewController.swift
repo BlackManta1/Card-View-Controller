0ea8a21ac1dcd5cabef9d57ca15b5f74661eb552
Saliou DJALO <djalosaliou@gmail.com>
2019-08-12 15:31:51 +0400

    Initial Commit

---
+++ Slide Out/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slide Out
+//
+//  Created by Saliou DJALO on 12/08/2019.
+//  Copyright © 2019 Saliou DJALO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
